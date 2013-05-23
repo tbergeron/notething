c5aac8cc075008d05135d5e8582e7362efd64d84
Tommy Bergeron <t.bergeron@gmail.com>
2013-05-23 00:23:38 -0400

    eh

--- js/main.js
+++ js/main.js
@@ -37,14 +37,16 @@ function edit() {
         $('#edit i').addClass('icon-white');
         
         tinymce.init({
+            inline: true,
             selector: "#editor",
             width: $(window).width() - 315,
             height: 500,
             plugins: [
-                 "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-                 "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-                 "save table contextmenu directionality emoticons template paste textcolor"
-            ]
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste moxiemanager"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
         });   
         
     }
