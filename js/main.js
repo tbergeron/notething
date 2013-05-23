33584dc2d331653095e4a71ed135b87d6cd397c5
Tommy Bergeron <t.bergeron@gmail.com>
2013-05-23 00:33:50 -0400

    eh

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function edit() {
             plugins: [
                 "advlist autolink lists link image charmap print preview anchor",
                 "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste moxiemanager"
+                "insertdatetime media table contextmenu paste"
             ],
             toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
         });   
