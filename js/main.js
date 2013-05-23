695322a37d55f1facac62e9ddf7c6cfb00c13ab8
Tommy Bergeron <t.bergeron@gmail.com>
2013-05-22 23:44:20 -0400

    yeah

---
+++ js/main.js
@@ -0,0 +1,30 @@
+Parse.initialize("Yeeo5YDal9Qmbzh4aCvvMLzUd3ralQW8qqOcx6Rn", "1QPi0d7rQLPM59c4FaP8VxwfMWJupQ8aNCcWNxNQ");
+
+$(function() {
+    $('#sidebar ul li:last').addClass('last');
+    
+    $('#sidebar ul li a').click(function() {
+        $('#sidebar ul li a').removeClass('selected'); 
+        $(this).addClass('selected'); 
+    });
+   
+   $('#edit').click(function() {
+        tinymce.init({
+            selector: "#editor",
+            width: $(window).width() - 315,
+            height: 500,
+            plugins: [
+                 "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+                 "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                 "save table contextmenu directionality emoticons template paste textcolor"
+            ]
+        });        
+   });
+                       
+    $(window).resize(function(){
+        $('#content').css('width', $(window).width() - 315);
+        $('.mce-tinymce').css('width', $(window).width() - 315);
+    });
+
+    $(window).resize();
+});
