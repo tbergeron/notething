f628f9245fd9b8dc330d921be233febf07e9d5c2
Tommy Bergeron <t.bergeron@gmail.com>
2013-05-23 10:00:39 -0400

    eh

--- js/data.js
+++ js/data.js
@@ -3,8 +3,16 @@ $(function() {
 
     var Page = Parse.Object.extend("Page");
     
-    var query = new Parse.Query(Page);
-    var collection = query.collection();
+    var collection = new PageCollection();
     
-    console.log(collection);
+    collection.fetch({
+        success: function(collection) {
+            collection.each(function(object) {
+                console.warn(object);
+            });
+        },
+        error: function(collection, error) {
+            // The collection could not be retrieved.
+        }
+    });
 });
