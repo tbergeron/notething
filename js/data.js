5063ed5d98d498bda529f906b360fc1b5eea82c6
Tommy Bergeron <t.bergeron@gmail.com>
2013-05-23 16:35:40 -0400

    yep

--- js/data.js
+++ js/data.js
@@ -1,6 +1,4 @@
 $(function() {
-    Parse.initialize("Yeeo5YDal9Qmbzh4aCvvMLzUd3ralQW8qqOcx6Rn", "1QPi0d7rQLPM59c4FaP8VxwfMWJupQ8aNCcWNxNQ");
-
     var Page = new Model('Page');
 
     GetPages = function(done) {
